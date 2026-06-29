5604db6d457f799a4e317232c161399e3021268e
khushi1327 <khushigupta1372005@gmail.com>
2026-06-29 15:29:25 +0530

    Update advance js

---
+++ Advance JS/advancejs.js
@@ -0,0 +1,27 @@
+function greet(){
+    console.log("perform makeing chai" )
+    console.log("1.boil water")
+    console.log("2.Gingger/Black peeper")
+    console.log("3.Add tea leaves")
+    console.log("4.Add Milk")
+    console.log(5."Add sugar")
+
+}
+greet()
+function greet(){
+    for(let i=1;i<=500;i++){
+        console.log("")
+    }
+}
+// Arrow Function-> 
+const greet=()=>{
+    console.log("This is an Arrow Function")
+}
+greet()
+//js sync ya async
+// bydefault js is sync in nature
+// but we can use asynchro keyboard
+console.log(1);
+console.log(2);
+console.log(3);
+
